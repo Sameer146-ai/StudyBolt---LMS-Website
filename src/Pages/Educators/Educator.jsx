b5a221a455f071283c0504b525d4e91bde0876bf
Sameer <sameerrahangdale63@gmail.com>
2025-08-13 12:53:26 +0530

    Educator DashBoard Completed , Footer of educator , navbar of educator

--- src/Pages/Educators/Educator.jsx
+++ src/Pages/Educators/Educator.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Navbar from '../../components/Educator/Navbar'
+import Sidebar from '../../components/Educator/Sidebar'
+import Footer from '../../components/Educator/Footer'
 
 function Educator() {
   return (
-    <div>
-      <h1>Educator</h1>
-      <div>
-      {<Outlet/>}
+    <div className='text-default min-h-screen bg-white'>
+      <Navbar/>
+      <div className='flex'>
+        <Sidebar/>
+        <div className='flex-1'>
+          {<Outlet/>}
+        </div>
     </div>
+    <Footer/>
     </div>
     
   )
--- src/components/Student/Loading.jsx
+++ src/components/Student/Loading.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from 'react';
 
 function Loading() {
   return (
-    <div>
-        <h1>Loding Section</h1>
+    <div className="flex flex-col items-center justify-center h-screen bg-white text-blue-800">
+      {/* Spinner */}
+      <div className="w-16 h-16 border-4 border-white/20 border-t-cyan-400 rounded-full animate-spin"></div>
+
+      {/* Loading Text */}
+      <h1 className="mt-4 text-lg tracking-wider animate-pulse">Loading...</h1>
     </div>
-  )
+  );
 }
 
-export default Loading
+export default Loading;
